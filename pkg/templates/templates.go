ca1cceb80f56ba1c717e6a10c9d24d014884fb2f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-12 03:46:53 +0000

    include viewData

--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -20,6 +20,22 @@ func NewTemplateManager(templateFiles embed.FS) (*TemplateManager, error) {
 	return &TemplateManager{templates: tmpl}, nil
 }
 
-func (tm *TemplateManager) Render(w io.Writer, name string, data interface{}, _ echo.Context) error {
-	return tm.templates.ExecuteTemplate(w, name, data)
+func (tm *TemplateManager) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	var viewData map[string]interface{}
+
+	if data != nil {
+		var ok bool
+		viewData, ok = data.(map[string]interface{})
+		if !ok {
+			viewData = make(map[string]interface{})
+			viewData["data"] = data
+		}
+	} else {
+		viewData = make(map[string]interface{})
+	}
+
+	// Add isAuthenticated to the view data
+	viewData["isAuthenticated"] = c.Get("isAuthenticated")
+
+	return tm.templates.ExecuteTemplate(w, name, viewData)
 }
