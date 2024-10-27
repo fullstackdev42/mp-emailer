fd53d6612dacdd4f3403c7a9ffeba551d7fc22e2
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-27 17:09:44 +0000

    Add open north api url to config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -8,57 +9,56 @@ import (
 	"github.com/jonesrussell/loggo"
 )
 
+// Config holds the application's configuration values.
 type Config struct {
-	AppDebug       string
-	AppEnv         string
-	AppPort        string
-	DBHost         string
-	DBName         string
-	DBPassword     string
-	DBPort         string
-	DBUser         string
-	JWTExpiry      string
-	JWTSecret      string
-	LogFile        string
-	LogLevel       string
-	MailgunAPIKey  string
-	MailgunDomain  string
-	MailpitHost    string
-	MailpitPort    string
-	MigrationsPath string
-	SessionName    string
-	SessionSecret  string
+	AppDebug                    string
+	AppEnv                      string
+	AppPort                     string
+	DBHost                      string
+	DBName                      string
+	DBPassword                  string
+	DBPort                      string
+	DBUser                      string
+	JWTExpiry                   string
+	JWTSecret                   string
+	LogFile                     string
+	LogLevel                    string
+	MailgunAPIKey               string
+	MailgunDomain               string
+	MailpitHost                 string
+	MailpitPort                 string
+	MigrationsPath              string
+	RepresentativeLookupBaseURL string
+	SessionName                 string
+	SessionSecret               string
 }
 
-// Load loads the configuration
+// Load loads the configuration from environment variables.
 func Load() (*Config, error) {
 	// Load .env file if it exists
 	_ = godotenv.Load()
 
 	config := &Config{
-		AppDebug:       getEnv("APP_DEBUG", "false"),
-		AppEnv:         getEnv("APP_ENV", "development"),
-		AppPort:        getEnv("APP_PORT", "8080"),
-		DBHost:         getEnv("DB_HOST", "localhost"),
-		DBName:         os.Getenv("DB_NAME"),
-		DBPassword:     os.Getenv("DB_PASSWORD"),
-		DBPort:         getEnv("DB_PORT", "3306"), // MariaDB default port
-		DBUser:         os.Getenv("DB_USER"),
-		JWTExpiry:      os.Getenv("JWT_EXPIRY"),
-		JWTSecret:      os.Getenv("JWT_SECRET"),
-		LogFile:        os.Getenv("LOG_FILE"),
-		LogLevel:       getEnv("LOG_LEVEL", "info"),
-		MailgunAPIKey:  getEnv("MAILGUN_API_KEY", ""),
-		MailgunDomain:  getEnv("MAILGUN_DOMAIN", ""),
-		MailpitHost:    getEnv("MAILPIT_HOST", "localhost"),
-		MailpitPort:    getEnv("MAILPIT_PORT", "1025"),
-		MigrationsPath: getEnv("MIGRATIONS_PATH", "migrations"),
-		SessionName:    getEnv("SESSION_NAME", "mpe"),
-		SessionSecret:  os.Getenv("SESSION_SECRET"),
-	}
-
-	if config.LogFile == "" {
-		config.LogFile = "mp-emailer.log" // Default value if not set
+		AppDebug:                    getEnv("APP_DEBUG", "false"),
+		AppEnv:                      getEnv("APP_ENV", "production"),
+		AppPort:                     getEnv("APP_PORT", "8080"),
+		DBHost:                      getEnv("DB_HOST", "localhost"),
+		DBName:                      getEnv("DB_NAME", "db"),
+		DBPassword:                  getEnv("DB_PASSWORD", "db"),
+		DBPort:                      getEnv("DB_PORT", "3306"), // MariaDB default port
+		DBUser:                      getEnv("DB_USER", "db"),
+		JWTExpiry:                   getEnv("JWT_EXPIRY", "1h"),
+		JWTSecret:                   getEnv("JWT_SECRET", ""),
+		LogFile:                     getEnv("LOG_FILE", "mp-emailer.log"),
+		LogLevel:                    getEnv("LOG_LEVEL", "info"),
+		MailgunAPIKey:               getEnv("MAILGUN_API_KEY", ""),
+		MailgunDomain:               getEnv("MAILGUN_DOMAIN", ""),
+		MailpitHost:                 getEnv("MAILPIT_HOST", "localhost"),
+		MailpitPort:                 getEnv("MAILPIT_PORT", "1025"),
+		MigrationsPath:              getEnv("MIGRATIONS_PATH", "migrations"),
+		RepresentativeLookupBaseURL: getEnv("REPRESENTATIVE_LOOKUP_BASE_URL", "https://represent.opennorth.ca"),
+		SessionName:                 getEnv("SESSION_NAME", "mpe"),
+		SessionSecret:               os.Getenv("SESSION_SECRET"),
 	}
 
 	// Validate required variables
@@ -69,9 +69,18 @@ func Load() (*Config, error) {
 		return nil, fmt.Errorf("SESSION_SECRET is not set in the environment")
 	}
 
+	// Print the Config struct
+	configJSON, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		fmt.Printf("Error marshaling config to JSON: %v\n", err)
+	} else {
+		fmt.Printf("Loaded configuration:\n%s\n", string(configJSON))
+	}
+
 	return config, nil
 }
 
+// getEnv gets an environment variable or returns a default value if not set.
 func getEnv(key, defaultValue string) string {
 	value := os.Getenv(key)
 	if value == "" {
@@ -80,6 +89,7 @@ func getEnv(key, defaultValue string) string {
 	return value
 }
 
+// GetLogLevel maps the log level string to loggo.Level.
 func (c *Config) GetLogLevel() loggo.Level {
 	switch c.LogLevel {
 	case "debug":
@@ -95,11 +105,13 @@ func (c *Config) GetLogLevel() loggo.Level {
 	}
 }
 
+// DatabaseDSN returns the Data Source Name for connecting to the database.
 func (c *Config) DatabaseDSN() string {
 	// DSN format specific to MariaDB
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
 }
 
+// IsDevelopment checks if the app is running in a development environment.
 func (c *Config) IsDevelopment() bool {
 	return c.AppEnv == "development"
 }
