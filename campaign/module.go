fb23d060f14ad041607ba63cba1303cc70a2f0b8
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-29 17:14:53 +0000

    Refactor shared module dependencies, add FlashHandler, refactor template rendering, and rename GetAllCampaigns

--- campaign/module.go
+++ campaign/module.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"github.com/fullstackdev42/mp-emailer/config"
 	"github.com/fullstackdev42/mp-emailer/email"
 	"github.com/fullstackdev42/mp-emailer/internal/database"
 	"github.com/fullstackdev42/mp-emailer/shared"
@@ -15,26 +16,74 @@ import (
 //nolint:gochecknoglobals
 var Module = fx.Options(
 	fx.Provide(
-		NewRepository,
-		NewClient,
-		fx.Annotate(NewRepresentativeLookupService,
-			fx.ParamTags(`name:"representativeLookupBaseURL"`, `name:"representativeLogger"`),
+		fx.Annotate(
+			NewRepository,
+			fx.As(new(RepositoryInterface)),
 		),
-		fx.Annotate(func(repo RepositoryInterface, validate *validator.Validate, logger loggo.LoggerInterface) (ServiceInterface, error) {
-			serviceParams := ServiceParams{Repo: repo, Validate: validate, Logger: logger}
-			serviceResult, err := NewService(serviceParams)
-			if err != nil {
-				return nil, err
-			}
-			return NewLoggingServiceDecorator(serviceResult.Service, logger), nil
-		}, fx.As(new(ServiceInterface))),
+		fx.Annotate(
+			func(repo RepositoryInterface, validate *validator.Validate, logger loggo.LoggerInterface) (ServiceInterface, error) {
+				serviceParams := ServiceParams{
+					Repo:     repo,
+					Validate: validate,
+					Logger:   logger,
+				}
+				serviceResult, err := NewService(serviceParams)
+				if err != nil {
+					return nil, err
+				}
+				return NewLoggingServiceDecorator(serviceResult.Service, logger), nil
+			},
+			fx.As(new(ServiceInterface)),
+		),
+		fx.Annotate(
+			func(logger loggo.LoggerInterface, cfg *config.Config) RepresentativeLookupServiceInterface {
+				return NewRepresentativeLookupService(cfg.RepresentativeLookupBaseURL, logger)
+			},
+			fx.As(new(RepresentativeLookupServiceInterface)),
+		),
+		fx.Annotate(
+			func(params ClientParams) (ClientInterface, error) {
+				return NewClient(params)
+			},
+			fx.As(new(ClientInterface)),
+		),
 		NewHandler,
 	),
 )
 
+// RepositoryParams for dependency injection
+type RepositoryParams struct {
+	fx.In
+	DB     *database.DB
+	Logger loggo.LoggerInterface
+}
+
+// RepositoryResult is the output struct for NewRepository
+type RepositoryResult struct {
+	fx.Out
+	Repository RepositoryInterface `group:"repositories"`
+}
+
 // NewRepository creates a new campaign repository
 func NewRepository(params RepositoryParams) (RepositoryInterface, error) {
-	return &Repository{db: params.DB}, nil
+	repo := &Repository{
+		db: params.DB,
+	}
+	return repo, nil
+}
+
+// ServiceParams for dependency injection
+type ServiceParams struct {
+	fx.In
+	Repo     RepositoryInterface
+	Validate *validator.Validate
+	Logger   loggo.LoggerInterface
+}
+
+// ServiceResult is the output struct for NewService
+type ServiceResult struct {
+	fx.Out
+	Service ServiceInterface
 }
 
 // NewService creates a new campaign service
@@ -61,9 +110,15 @@ type HandlerParams struct {
 	TemplateRenderer            *shared.CustomTemplateRenderer
 }
 
+// HandlerResult is the output struct for NewHandler
+type HandlerResult struct {
+	fx.Out
+	Handler *Handler
+}
+
 // NewHandler initializes a new Handler
-func NewHandler(params HandlerParams) *Handler {
-	return &Handler{
+func NewHandler(params HandlerParams) (HandlerResult, error) {
+	handler := &Handler{
 		service:                     params.Service,
 		logger:                      params.Logger,
 		representativeLookupService: params.RepresentativeLookupService,
@@ -72,50 +127,7 @@ func NewHandler(params HandlerParams) *Handler {
 		errorHandler:                params.ErrorHandler,
 		templateRenderer:            params.TemplateRenderer,
 	}
-}
-
-// ServiceParams for dependency injection
-type ServiceParams struct {
-	fx.In
-	Repo     RepositoryInterface
-	Validate *validator.Validate
-	Logger   loggo.LoggerInterface
-}
-
-// RepositoryParams for dependency injection
-type RepositoryParams struct {
-	fx.In
-	DB     *database.DB
-	Logger loggo.LoggerInterface
-}
-
-// ClientParams for dependency injection
-type ClientParams struct {
-	fx.In
-	Logger        loggo.LoggerInterface
-	LookupService RepresentativeLookupServiceInterface
-}
-
-// ServiceResult is the output struct for NewService
-type ServiceResult struct {
-	fx.Out
-	Service ServiceInterface
-}
-
-// HandlerResult is the output struct for NewHandler
-type HandlerResult struct {
-	fx.Out
-	Handler *Handler
-}
-
-// RegisterRoutes registers the campaign routes
-func RegisterRoutes(h *Handler, e *echo.Echo) {
-	e.GET("/campaign", h.GetCampaigns)
-	e.POST("/campaign", h.CreateCampaign)
-	e.GET("/campaign/:id", h.CampaignGET)
-	e.PUT("/campaign/:id", h.EditCampaign)
-	e.DELETE("/campaign/:id", h.DeleteCampaign)
-	e.POST("/campaign/:id/send", h.SendCampaign)
+	return HandlerResult{Handler: handler}, nil
 }
 
 // NewRepresentativeLookupService creates a new instance of RepresentativeLookupService
@@ -126,6 +138,13 @@ func NewRepresentativeLookupService(baseURL string, logger loggo.LoggerInterface
 	}
 }
 
+// ClientParams for dependency injection
+type ClientParams struct {
+	fx.In
+	Logger        loggo.LoggerInterface
+	LookupService RepresentativeLookupServiceInterface
+}
+
 // NewClient creates a new instance of ClientInterface
 func NewClient(params ClientParams) (ClientInterface, error) {
 	client := &DefaultClient{
@@ -133,4 +152,16 @@ func NewClient(params ClientParams) (ClientInterface, error) {
 		lookupService: params.LookupService,
 	}
 	return client, nil
+}
+
+// RegisterRoutes registers the campaign routes
+func RegisterRoutes(h *Handler, e *echo.Echo) {
+	e.GET("/campaigns", h.GetCampaigns)
+
+	campaignGroup := e.Group("/campaign")
+	campaignGroup.POST("", h.CreateCampaign)
+	campaignGroup.GET("/:id", h.CampaignGET)
+	campaignGroup.PUT("/:id", h.EditCampaign)
+	campaignGroup.DELETE("/:id", h.DeleteCampaign)
+	campaignGroup.POST("/:id/send", h.SendCampaign)
 }
